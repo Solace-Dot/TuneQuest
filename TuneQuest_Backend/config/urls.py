28ee91a1db71b33c00c03956d223b6aef99ca2ab
monicarawr <monicarosebautista3@gmail.com>
2026-03-15 15:19:20 +0800

    hmm

--- TuneQuest_Backend/config/urls.py
+++ TuneQuest_Backend/config/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('ai/', include('ai.urls')),
     path("admin/", admin.site.urls),
     path("api/auth/", include("accounts.urls")),
